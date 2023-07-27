3e2cf354d384279ffdbeeb0b1e6efc765daaa8b1
tivt <tivt231@gmail.com>
2023-07-27 13:54:06 -0300

    feat: created RefreshTokenMap class(sometype of hashmap), some quick and dirty console.log checks, needs unit tests

---
+++ src/infra/utils/refresh-tokens-dirty-testing.ts
@@ -0,0 +1,29 @@
+import { RefreshTokensMap } from './refresh-tokens-map';
+
+const myRefreshMap = new RefreshTokensMap();
+myRefreshMap.set('0', 'token0');
+myRefreshMap.set('1', 'token1');
+myRefreshMap.set('1', 'token1_changed');
+myRefreshMap.set('2', 'token2');
+myRefreshMap.set('8', 'token8');
+myRefreshMap.set('16', 'token16');
+myRefreshMap.set('7', 'token7');
+console.log();
+console.log('BEFORE GROWING');
+myRefreshMap.print();
+myRefreshMap.set('9', 'token9');
+console.log();
+console.log('AFTER GROWING');
+myRefreshMap.print();
+console.log(
+  myRefreshMap.get('1'),
+  myRefreshMap.get('8'),
+  myRefreshMap.get('16'),
+  myRefreshMap.get('22'),
+);
+myRefreshMap.remove('22');
+myRefreshMap.remove('16');
+myRefreshMap.remove('1');
+myRefreshMap.remove('0');
+myRefreshMap.remove('9');
+myRefreshMap.print();
---
+++ src/infra/utils/refresh-tokens-map.ts
@@ -0,0 +1,261 @@
+type RefreshNode = {
+  key: string;
+  token: string;
+  createdAt: Date;
+  prevIterator?: RefreshNode;
+  nextIterator?: RefreshNode;
+  prevHash?: RefreshNode;
+  nextHash?: RefreshNode;
+};
+
+export class RefreshTokensMap {
+  private refreshMap: Array<[head?: RefreshNode, tail?: RefreshNode]>;
+  private dataLength;
+
+  private head?: RefreshNode;
+  private tail?: RefreshNode;
+
+  constructor(
+    private capacity = 8,
+    private growLoadFactor = 0.7,
+    private shrinkLoadFactor = 0.3,
+    private sizeChangeMultiplier = 2,
+    private minimumSize = 8,
+  ) {
+    this.refreshMap = Array.from({ length: this.capacity }, () => [
+      undefined,
+      undefined,
+    ]);
+    this.dataLength = 0;
+    this.head = this.tail = undefined;
+  }
+
+  set(key: string, token: string): void {
+    this.growMap();
+
+    const idx = this.hashKey(key);
+    const node = this.makeNode(key, token);
+    const hashPos = this.refreshMap[idx];
+
+    const nodeExists = this.findNode(hashPos[0], key);
+    if (!nodeExists) {
+      this.dataLength++;
+      this.appendIterator(node);
+      this.appendHash(node, idx);
+      return;
+    }
+
+    nodeExists.token = token;
+  }
+
+  get(key: string): { key: string; token: string } | undefined {
+    const idx = this.hashKey(key);
+    const headOfIdx = this.refreshMap[idx][0];
+
+    if (!headOfIdx) {
+      return;
+    }
+
+    const node = this.findNode(headOfIdx, key);
+    if (!node) {
+      return;
+    }
+
+    return { key: node.key, token: node.token };
+  }
+
+  remove(key: string): { key: string; token: string } | undefined {
+    this.shrinkMap();
+
+    const idx = this.hashKey(key);
+    const headOfIdx = this.refreshMap[idx][0];
+
+    if (!headOfIdx) {
+      return;
+    }
+
+    const node = this.findNode(headOfIdx, key);
+    if (!node) {
+      return;
+    }
+
+    this.dataLength--;
+    this.breakLink(node, idx);
+    return { key: node.key, token: node.token };
+  }
+
+  print(): void {
+    console.log();
+    console.log(
+      `dataLength: ${this.dataLength} | `,
+      `loadFactor: ${this.dataLength / this.capacity}`,
+    );
+
+    console.log();
+    console.log('ITERATOR');
+    let iterator = '';
+    let currIterator = this.head;
+    while (currIterator) {
+      iterator += currIterator?.key + ' -> ';
+      currIterator = currIterator.nextIterator;
+    }
+    console.log(iterator);
+
+    console.log();
+
+    console.log('HASHMAP');
+    for (let i = 0; i < this.capacity; i++) {
+      let curr = '';
+      let currNode = this.refreshMap[i][0];
+      if (!currNode) {
+        curr = 'undefined';
+      }
+      while (currNode) {
+        curr += currNode?.key + ' -> ';
+        currNode = currNode.nextHash;
+      }
+      console.log(curr);
+    }
+  }
+
+  printMap(): void {
+    console.log(this.refreshMap);
+  }
+
+  private appendIterator(node: RefreshNode): void {
+    const oldTail = this.tail;
+    if (!oldTail) {
+      this.head = this.tail = node;
+      return;
+    }
+
+    this.tail = node;
+    node.prevIterator = oldTail;
+    oldTail.nextIterator = node;
+  }
+
+  private appendHash(node: RefreshNode, idx: number): void {
+    const hashPos = this.refreshMap[idx];
+    const oldTail = hashPos[1];
+    if (!oldTail) {
+      hashPos[0] = hashPos[1] = node;
+      return;
+    }
+
+    hashPos[1] = node;
+    node.prevHash = oldTail;
+    oldTail.nextHash = node;
+  }
+
+  private breakLink(node: RefreshNode, idx: number): void {
+    const hashPos = this.refreshMap[idx];
+
+    if (this.dataLength === 1) {
+      this.head = this.tail = undefined;
+      hashPos[0] = hashPos[1] = undefined;
+      return;
+    }
+
+    // ITERATOR LINK BREAK
+    const prevIterator = node.prevIterator;
+    const nextIterator = node.nextIterator;
+    if (prevIterator) {
+      prevIterator.nextIterator = nextIterator;
+    }
+    if (nextIterator) {
+      nextIterator.prevIterator = prevIterator;
+    }
+    node.prevIterator = node.nextIterator = undefined;
+    if (!prevIterator) {
+      this.head = nextIterator;
+    }
+    if (!nextIterator) {
+      this.tail = prevIterator;
+    }
+
+    // HASHMAP LINK BREAK
+    const prevHash = node.prevHash;
+    const nextHash = node.nextHash;
+    if (prevHash) {
+      prevHash.nextHash = nextHash;
+    }
+    if (nextHash) {
+      nextHash.prevHash = prevHash;
+    }
+    node.prevHash = node.nextHash = undefined;
+    if (!prevHash) {
+      hashPos[0] = nextHash;
+    }
+    if (!nextHash) {
+      hashPos[1] = prevHash;
+    }
+  }
+
+  private findNode(
+    head: RefreshNode | undefined,
+    key: string,
+  ): RefreshNode | undefined {
+    let curr: RefreshNode | undefined = head;
+    while (curr && curr.key !== key) {
+      curr = curr.nextHash;
+    }
+    return curr;
+  }
+
+  private hashKey(key: string): number {
+    const idx = key.charCodeAt(0) % this.capacity;
+    return idx;
+  }
+
+  private growMap(): void {
+    const needGrowing = this.dataLength / this.capacity > this.growLoadFactor;
+    if (!needGrowing) {
+      return;
+    }
+
+    this.capacity = Math.floor(this.capacity * this.sizeChangeMultiplier);
+    this.refreshMap = Array.from({ length: this.capacity }, () => [
+      undefined,
+      undefined,
+    ]);
+
+    let curr = this.head;
+    while (curr) {
+      const hashIdx = this.hashKey(curr?.key);
+      this.appendHash(curr, hashIdx);
+      curr = curr.nextIterator;
+    }
+  }
+
+  private shrinkMap(): void {
+    const needShrinking =
+      this.dataLength / this.capacity < this.shrinkLoadFactor;
+    const canShrink =
+      Math.floor(this.capacity / this.sizeChangeMultiplier) >= this.minimumSize;
+    if (!needShrinking || !canShrink) {
+      return;
+    }
+
+    this.capacity = Math.floor(this.capacity / this.sizeChangeMultiplier);
+    this.refreshMap = Array.from({ length: this.capacity }, () => [
+      undefined,
+      undefined,
+    ]);
+
+    let curr = this.head;
+    while (curr) {
+      const hashIdx = this.hashKey(curr?.key);
+      this.appendHash(curr, hashIdx);
+      curr = curr.nextIterator;
+    }
+  }
+
+  private makeNode(key: string, token: string): RefreshNode {
+    const node = {
+      key,
+      token,
+      createdAt: new Date(),
+    } as RefreshNode;
+    return node;
+  }
+}
